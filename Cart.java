84ec0c227ece389e8cc56424c3ad1a8f6cbd32ab
MONISHAMARIMUTHU <145739509+MONISHAMARIMUTHU@users.noreply.github.com>
2026-04-01 18:09:39 +0530

    Add files via upload

---
+++ Cart.java
@@ -0,0 +1,33 @@
+package com.backend.retail_website.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @OneToMany(
+        mappedBy = "cart",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true   // 🔥 THIS IS THE KEY FIX
+    )
+    @JsonManagedReference
+    private List<CartItem> items = new ArrayList<>();
+}
---
+++ CartItem.java
@@ -0,0 +1,31 @@
+package com.backend.retail_website.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+ import lombok.Data;
+
+@Entity
+@Data
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    @JsonBackReference
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
